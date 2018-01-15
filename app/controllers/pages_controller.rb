5c81cdd5071eef1207412e869696d7d10092e095
Ian Purdom <ianpurdom75@gmail.com>
2018-01-15 21:48:19 +0100

    seeds done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  # skip_before_action :authenticate_user! #, only: [:home]
+  # si commenté => même la home page nécéssite une authentification
 
   def home
     @city = City.where(user: current_user).first
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "cleaning seeds..."
+
 Deputy.destroy_all
+City.destroy_all
+Program.destroy_all
+Category.destroy_all
 User.destroy_all
 
 puts "creating seeds..."
 
-yann = User.create(first_name: "yan", last_name: "de Catheu", email: "yann@mail.com", password: '123456')
+puts "creating users..."
+User.create!(first_name: "Charles", last_name: "Bazin", email: "charles@mail.com", photo: "image/upload/v1515578859/q38jatr8rzd673e61tu4.jpg", password: '123456')
+
+
+puts "creating deputies..."
 
 deputies = [
 { first_name: "Laurent",
@@ -35,9 +43,50 @@ deputies = [
 }
 ]
 
-deputies.each_with_index do |deputy, index|
+deputies.each_with_index do |deputy|
   new_deputy = Deputy.new(deputy)
+  new_deputy.photo =  Rails.root.join("db/images/#{new_deputy.last_name}.jpeg").open
   new_deputy.save
+end
+
+puts "creating city..."
+
+new_city = City.new(
+  zip_code: 27130,
+  departement: "l'Eure",
+  region: "Normandie",
+  intercommunalite: "Communauté de communes Normandie Sud Eure",
+  population: 320,
+  density: 25,
+  debt: nil,
+  current_maire: "Philippe Obadia",
+  name: "Les Barils"
+)
+new_city.user = User.find_by(first_name: "Charles")
+new_city.photo_1 = Rails.root.join("db/images/mairie_1.jpg").open
+new_city.photo_1 = Rails.root.join("db/images/mairie_2.jpg").open
+new_city.save
+
+puts "creating categories..."
+
+categories = ["Finance", "Urbanisme", "Sécurité"]
+
+categories.each do |category|
+  Category.create!(name: category)
+end
+
+puts "creating programs & categories..."
+file = "db/programs.yml"
+programs = YAML.load(open(file).read)
+
+n = 0
+programs["programs"].each do |program|
+  # Category.create!(categories[n])
+  u = Program.new(program)
+  u.category = Category.find_by(name: categories[n])
+  p u
+  u.save
+  n += 1
 end
 
 puts "seeds done"
